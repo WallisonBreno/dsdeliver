2cf87bcb06bf664e350028871fd25be2315843d6
unknown <wallisonbreno123@gmail.com>
2021-04-12 02:58:47 -0300

    Home created

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css';
+import Home from './Home';
 import Navbar from './Navbar';
 
 function App() {
   return (
-    <div className="App">
+    <>
         <Navbar/>
-    </div>
+        <Home/>
+    </>
   );
 }
 
